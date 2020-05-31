2b2df736c8e5829eb5472b43207dc1304b4488cc
Untung So Andryanto <untungsoandryanto@gmail.com>
2020-06-01 04:18:35 +0700

    iinit

--- Player/Player.gd
+++ Player/Player.gd
@@ -4,8 +4,12 @@ const ACCELERATION = 500
 const MAX_SPEED = 80
 const FRICTION = 500
 
-
 var velocity = Vector2.ZERO
+
+onready var animation_player = $AnimationPlayer
+onready var animation_tree = $AnimationTree
+onready var animation_state = animation_tree.get("parameters/playback")
+
 
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
@@ -14,8 +18,12 @@ func _physics_process(delta):
 	input_vector = input_vector.normalized()
 
 	if input_vector != Vector2.ZERO:
+		animation_tree.set('parameters/Idle/blend_position', input_vector)
+		animation_tree.set('parameters/Run/blend_position', input_vector)	
+		animation_state.travel("Run")
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else :
+		animation_state.travel("Idle")		
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 
 	velocity = move_and_slide(velocity)
